42b84c905bc788cef400f6c105ebf8cce0c2198a
Aashish <Aashish.shrivastava2015@gmail.com>
2024-08-20 21:44:45 +0530

    feat: Env setup

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,12 @@
 // Its use for env setup 
+import DotenvFlow from 'dotenv-flow';
+DotenvFlow.config()
+
+export default {
+    //General  
+    ENV: process.env.ENV,
+    PORT: process.env.PORT,
+    SERVER_URL: process.env.SERVER_URL,
+    //Database 
+    DATABASE_URL: process.env.DATABASE_URL,
+}
