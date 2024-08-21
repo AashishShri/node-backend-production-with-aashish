2461364c50f39f2fce961ec338a3092f191af8e5
Aashish <Aashish.shrivastava2015@gmail.com>
2024-08-21 21:46:20 +0530

    feat: Express setup

--- src/constant/application.ts
+++ src/constant/application.ts
@@ -1,1 +1,5 @@
 // Its use for constant value for application
+export enum EApplicationEnvironment {
+    PRODUCTION = 'production',
+    DEVELOPMENT = 'development'
+}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,41 @@
-//import config from './config/config';
+import config from './config/config';
+import app from './app'
+
+const server = app.listen(config.PORT)
+
+
+    ; (() => {
+       
+        try {
+            // Database Connection
+            //const connection = await databaseService.connect()
+            // eslint-disable-next-line no-console
+            console.info(`DATABASE_CONNECTION`, {
+                meta: {
+                    CONNECTION_NAME: config.SERVER_URL
+                }
+            })
+
+
+
+// eslint-disable-next-line no-console
+            console.info(`APPLICATION_STARTED`, {
+                meta: {
+                    PORT: config.PORT,
+                    SERVER_URL: config.SERVER_URL
+                }
+            })
+        } catch (err) {
+            // eslint-disable-next-line no-console
+            console.error(`APPLICATION_ERROR`, { meta: err })
+
+            server.close((error) => {
+                if (error) {
+                    // eslint-disable-next-line no-console
+                    console.error(`APPLICATION_ERROR`, { meta: error })
+                }
+
+                process.exit(1)
+            })
+        }
+    })()
