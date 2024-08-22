b3406a495c3fedf266d8f5e9fd5228c5677d22a1
Aashish <Aashish.shrivastava2015@gmail.com>
2024-08-22 21:51:07 +0530

    feat: Winston mongodb logger

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,16 +1,16 @@
 import util from 'util'
+import 'winston-mongodb'
 import { createLogger, format, transports } from 'winston'
 import { ConsoleTransportInstance, FileTransportInstance } from 'winston/lib/winston/transports'
 import config from '../config/config'
 import { EApplicationEnvironment } from '../constant/application'
 import path from 'path'
+import { red, blue, yellow, green, magenta } from 'colorette'
 import * as sourceMapSupport from 'source-map-support'
-import { blue, green, red, yellow, magenta } from 'colorette'
+import { MongoDBTransportInstance } from 'winston-mongodb'
 
-
+// Linking Trace Support
 sourceMapSupport.install()
-
-
 
 const colorizeLevel = (level: string) => {
     switch (level) {
@@ -101,24 +101,27 @@ const FileTransport = (): Array<FileTransportInstance> => {
     ]
 }
 
-// const MongodbTransport = (): Array<MongoDBTransportInstance> => {
-//     return [
-//         new transports.MongoDB({
-//             level: 'info',
-//             db: config.DATABASE_URL as string,
-//             metaKey: 'meta',
-//             expireAfterSeconds: 3600 * 24 * 30,
-//             options: {
-//                 useUnifiedTopology: true
-//             },
-//             collection: 'application-logs'
-//         })
-//     ]
-// }
+const MongodbTransport = (): Array<MongoDBTransportInstance> => {
+     
+    return [
+         
+        new transports.MongoDB({
+            level: 'info',
+            db: config.DATABASE_URL as string,
+            metaKey: 'meta',
+            expireAfterSeconds: 3600 * 24 * 30,
+            options: {
+                useUnifiedTopology: true
+            },
+            collection: 'application-logs'
+        })
+    ]
+}
 
 export default createLogger({
     defaultMeta: {
         meta: {}
     },
-    transports: [...FileTransport(), ...consoleTransport()]
+     
+    transports: [...FileTransport(), ...MongodbTransport(), ...consoleTransport()]
 })
